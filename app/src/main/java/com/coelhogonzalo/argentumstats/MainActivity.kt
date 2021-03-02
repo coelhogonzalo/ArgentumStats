d38d0939bd862af6a985782ef7ae15c1831ce2cd
LAPTOP-R3MGKV5C\Intermedia <coelhogonza@gmail.com>
2021-03-02 10:39:03 -0300

    First commit

---
+++ app/src/main/java/com/coelhogonzalo/argentumstats/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.coelhogonzalo.argentumstats
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //TESTING IMPORTING AN EXISTING PROJECT
+    }
+}
