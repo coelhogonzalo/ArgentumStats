3ed59fd6e6e7204900eba9b841e6f6b6956d289e
LAPTOP-R3MGKV5C\Intermedia <coelhogonza@gmail.com>
2021-03-03 12:27:08 -0300

    Testing push with different credential

--- app/src/main/java/com/coelhogonzalo/argentumstats/MainActivity.kt
+++ app/src/main/java/com/coelhogonzalo/argentumstats/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //TESTING COMMIT AND PUSH WITH DIFFERENT CREDENTIAL
+        //TESTING COMMIT AND PUSH WITH DIFFERENT CREDENTIA
     }
 }
